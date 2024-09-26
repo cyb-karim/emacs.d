022233ed800b0aa019bc421f7d15dd8fc48522da
Karim Nassar <-g>
2024-09-25 20:19:31 -0500

    update magit setup

--- custom.el
+++ custom.el
@@ -12,6 +12,7 @@
  '(ivy-magic-tilde nil)
  '(magit-diff-arguments
    '("--ignore-space-change" "--ignore-all-space" "--no-ext-diff"))
+ '(magit-diff-refine-hunk 'all)
  '(magit-status-sections-hook
    '(magit-insert-status-headers magit-insert-merge-log magit-insert-rebase-sequence magit-insert-am-sequence magit-insert-sequencer-sequence magit-insert-bisect-output magit-insert-bisect-rest magit-insert-bisect-log magit-insert-untracked-files magit-insert-unstaged-changes magit-insert-staged-changes magit-insert-stashes magit-insert-unpushed-to-pushremote magit-insert-unpushed-to-upstream-or-recent magit-insert-unpulled-from-pushremote magit-insert-unpulled-from-upstream magit-insert-local-branches))
  '(org-hide-emphasis-markers t)
