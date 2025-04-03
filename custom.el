dafd65ba979c303ff2bc51176b658c5b4ec20a87
Karim Nassar <-g>
2025-04-03 09:32:34 -0500

    rust cleanups

--- custom.el
+++ custom.el
@@ -17,12 +17,23 @@
    '(magit-insert-status-headers magit-insert-merge-log magit-insert-rebase-sequence magit-insert-am-sequence magit-insert-sequencer-sequence magit-insert-bisect-output magit-insert-bisect-rest magit-insert-bisect-log magit-insert-untracked-files magit-insert-unstaged-changes magit-insert-staged-changes magit-insert-stashes magit-insert-unpushed-to-pushremote magit-insert-unpushed-to-upstream-or-recent magit-insert-unpulled-from-pushremote magit-insert-unpulled-from-upstream magit-insert-local-branches))
  '(org-hide-emphasis-markers t)
  '(package-selected-packages
-   '(edit-indirect gptel yasnippet yaml-mode omnisharp csharp-mode htmlize csv-mode isortify sql-indent sqlind ob-sql-mode company-nginx nginx-mode python-black ivy-xref prescient hydra ivy ledger-mode json-mode json-snatcher json-reformat company-lsp dap-mode lsp-ui lsp-mode js2-mode web-mode prettier-js string-inflection dockerfile-mode which-key use-package-ensure-system-package rg magit ivy-prescient ivy-hydra counsel company-prescient))
+   '(ob-rust rust-mode rustic cargo poetry exec-path-from-shell edit-indirect gptel yasnippet yaml-mode htmlize csv-mode isortify sql-indent sqlind ob-sql-mode company-nginx nginx-mode python-black ivy-xref prescient hydra ivy ledger-mode json-mode json-snatcher json-reformat company-lsp dap-mode lsp-ui lsp-mode js2-mode web-mode prettier-js string-inflection dockerfile-mode which-key use-package-ensure-system-package rg magit ivy-prescient ivy-hydra counsel company-prescient))
  '(python-fill-docstring-style 'django)
  '(python-shell-interpreter "python3")
  '(ripgrep-arguments '("--no-ignore"))
  '(safe-local-variable-values
-   '((eval add-hook 'after-save-hook #'org-babel-tangle nil t)))
+   '((eval setq exec-path
+           (cons
+            (concat
+             (getenv "HOME")
+             "/.cargo/bin")
+            exec-path))
+     (eval setenv "PATH"
+           (concat
+            (getenv "HOME")
+            "/.cargo/bin:"
+            (getenv "PATH")))
+     (eval add-hook 'after-save-hook #'org-babel-tangle nil t)))
  '(sql-mode-hook '(sqlind-minor-mode))
  '(typescript-indent-level 2))
 (custom-set-faces
@@ -38,6 +49,7 @@
  '(font-lock-doc-face ((t (:foreground "dark gray"))))
  '(font-lock-function-name-face ((t (:foreground "#0099ff"))))
  '(font-lock-keyword-face ((t (:foreground "#009da6"))))
+ '(font-lock-preprocessor-face ((t (:foreground "DeepSkyBlue4"))))
  '(font-lock-string-face ((t (:foreground "dark sea green"))))
  '(highlight ((t (:background "DarkSlateGray4" :foreground "#002b36"))))
  '(ivy-current-match ((((class color) (background dark)) (:background "#1a4b77" :foreground "yellow" :extend t)) (((class color) (background dark)) (:background "#65a7e2" :foreground "black" :extend t))))
